ae1829a996130830f830e9d0764726b5302d154d
bjj3036 <baebo0823@naver.com>
2017-12-18 16:02:54 +0900

    Login

--- app/src/main/java/com/dgsw/doorlock/activity/Login.java
+++ app/src/main/java/com/dgsw/doorlock/activity/Login.java
@@ -1,27 +1,47 @@
 package com.dgsw.doorlock.activity;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import com.dgsw.doorlock.R;
+import com.dgsw.doorlock.tool.Preference;
 
 public class Login extends AppCompatActivity {
+    private boolean save_check;
+    private String id = null;
+    private EditText InputID=null;
+    private EditText InputPW=null;
+    private Button LoginBtn=null;
+    private TextView RegisterBtn=null;
+    private CheckBox save_id=null;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        EditText InputID = findViewById(R.id.InputID);
-        EditText InputPW = findViewById(R.id.InputPW);
+        InputID = findViewById(R.id.InputID);
+        InputPW = findViewById(R.id.InputPW);
+        LoginBtn = findViewById(R.id.LoginBtn);
+        save_id = findViewById(R.id.save_id);
+        RegisterBtn = findViewById(R.id.RegisterBtn);
 
-        Button LoginBtn = findViewById(R.id.LoginBtn);
-        TextView RegisterBtn = findViewById(R.id.RegisterBtn);
+        LoginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        if(new Preference(getApplicationContext()).getBoolean("SAVE_ID",false))
+            InputID.setText(new Preference(getApplicationContext()).getString("ID",null));
 
         RegisterBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -30,5 +50,24 @@ public class Login extends AppCompatActivity {
                 Login.this.startActivity(registerIntent);
             }
         });
+
+        save_id.setChecked(new Preference(getApplicationContext()).getBoolean("SAVE_ID", false));
+
+        save_id.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                save_check = save_id.isChecked();
+            }
+        });
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        new Preference(getApplicationContext()).putBoolean("SAVE_ID", save_check);
+        if (save_check) {
+            id=InputID.getText().toString();
+            new Preference(getApplicationContext()).putString("ID", id);
+        }
     }
 }
