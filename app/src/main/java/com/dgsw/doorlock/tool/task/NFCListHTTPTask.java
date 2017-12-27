2e9f7d772599f433cc9c11d7390dda80ac5abbb5
bjj3036 <baebo0823@naver.com>
2017-12-27 18:51:14 +0900

    nfc 찍을 수 있는 시간인건만 보여주기

--- app/src/main/java/com/dgsw/doorlock/tool/task/NFCListHTTPTask.java
+++ app/src/main/java/com/dgsw/doorlock/tool/task/NFCListHTTPTask.java
@@ -13,7 +13,10 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 import static com.dgsw.doorlock.activity.Main.IP_ADDRESS;
 
@@ -65,21 +68,45 @@ public class NFCListHTTPTask extends AsyncTask<EntryInfo, Integer, ArrayList<NFC
             String buf = br.readLine();
             org.json.JSONObject jo;
             org.json.JSONArray ja = new org.json.JSONArray(buf);
+            Date current=new Date();
+            Date outCompare=null,inCompare=null;
+            SimpleDateFormat form=new SimpleDateFormat("yyyy-MM-dd-kk:mm:ss");
+            StringBuilder outString=null,inString=null,dayString=null;
             for (int i = 0; i < ja.length(); i++) {
                 jo = ja.getJSONObject(i);
                 if("1".equals(jo.get("state").toString())&&id.equals(jo.get("userId").toString())) {
-                    StringBuilder date = new StringBuilder(jo.get("date").toString());
-                    date.delete(10, date.length());
+                    outString=new StringBuilder(jo.get("outTime").toString().replace('T','-'));
+                    outString.delete(19,outString.length());
+                    outString.delete(0,10);
 
-                    StringBuilder in_Time = new StringBuilder(jo.get("inTime").toString());
-                    in_Time.delete(0, 11);
-                    in_Time.delete(8, in_Time.length());
+                    inString=new StringBuilder(jo.get("inTime").toString().replace('T','-'));
+                    inString.delete(19,inString.length());
+                    inString.delete(0,10);
 
-                    StringBuilder out_Time = new StringBuilder(jo.get("outTime").toString());
-                    out_Time.delete(0, 11);
-                    out_Time.delete(8, out_Time.length());
+                    dayString=new StringBuilder(jo.get("date").toString());
+                    dayString.delete(10,dayString.length());
 
-                    nfcInfos.add(new NFCInfo(date.toString(), in_Time.toString(), out_Time.toString(), jo.get("rfid").toString()));
+                    try {
+                        outCompare=form.parse(dayString.toString()+outString.toString());
+                        inCompare=form.parse(dayString.toString()+inString.toString());
+                    } catch (ParseException e) {
+                        Log.e("Pares",outString.toString()+" "+inString.toString());
+                    }
+
+                    if(outCompare.compareTo(current)>=0&&inCompare.compareTo(current)<=0) {
+                        StringBuilder date = new StringBuilder(jo.get("date").toString());
+                        date.delete(10, date.length());
+
+                        StringBuilder in_Time = new StringBuilder(jo.get("inTime").toString());
+                        in_Time.delete(0, 11);
+                        in_Time.delete(8, in_Time.length());
+
+                        StringBuilder out_Time = new StringBuilder(jo.get("outTime").toString());
+                        out_Time.delete(0, 11);
+                        out_Time.delete(8, out_Time.length());
+
+                        nfcInfos.add(new NFCInfo(date.toString(), in_Time.toString(), out_Time.toString(), jo.get("rfid").toString()));
+                    }
                 }
             }
 
